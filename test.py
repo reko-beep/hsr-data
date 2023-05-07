68474821df44f48b365bf1634c58c1d257935334
Reko <alitahir358@gmail.com>
2023-05-07 17:40:59 +0500

    fetch all data raw data.py

--- test.py
+++
@@ -1,9 +0,0 @@
-from main import SRSClient
-from constants import Types, Languages
-
-from json import dump
-
-client = SRSClient()
-r = client.get_all_items(Types.CHARACTERS)[0]
-c = client.get_all_character_details(r)
-print(c)
