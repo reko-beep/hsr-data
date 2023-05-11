743b145b629b753339e9d4c786094ff1dd2cbd72
Best <40067018+BobyClaws@users.noreply.github.com>
2023-05-11 22:21:15 +0530

    updated trace model

--- datamodels/trace.py
+++ datamodels/trace.py
@@ -1,17 +1,13 @@
 
-from typing import Optional
-from pydantic import BaseModel, validator, Field, Extra
+from typing import Optional, Union, List, NewType
+from pydantic import BaseModel, validator, Field, Extra, ValidationError
 
 from enum import Enum
 
-class Type(Enum):
-    """type of trace (Stat Bonus, Skill, Bonus Ability)"""
-    BONUS_ABILITY = 1
-    STAT_BONUS = 2
-    SKILL = 3
+class Material(BaseModel):
+    pass
 
-
-class UnlockCriteria(BaseModel):
+class UnlockPrerequisite(BaseModel):
     """criteria to satisfy before this trace can be unlocked."""
     # character ascension required.
     ascension: Optional[int]
@@ -22,22 +18,46 @@ class UnlockCriteria(BaseModel):
   
     
 
+class BonusAbility:
+    # name of the trace.
+    name : str
+    # description of the trace.
+    description: Optional[str]
+    # trace level.
+    level: int = 1
+    # list of materials required to activate the trace.
+    activation_mats: List[(Material, int)]
+    # criteria to satisfy before this trace can be unlocked.
+    unlock_prerequisite: Optional[UnlockPrerequisite]
 
+
+    # @validator
+    # def ensure_level_one(cls, level):
+    #     if level is not 1:
+    #         raise ValidationError("Bonus Ability's level can only be equal to 1")
+
+StatBonus = NewType('StatBonus', BonusAbility)
+
+
+class LevelScaling(BaseModel):
+    level: int
+    upgrade_mats: List[(Material, int)]
+    description: str
+    
 
 # TODO: decide all the parameters
-class Trace(BaseModel):
+class Skill(BaseModel):
     """Traces possessed by the `Character`"""
     # name of the trace.
     name : str
-    # type of trace.
-    type : Type
-    # description of the trace.
-    description: Optional[str]
-    # criteria to satisfy before this trace can be unlocked.
-    unlock_criteria: Optional[UnlockCriteria]
-    # trace level.
-    level: Optional[int]
+    # how the trace scales with level
+    level_scaling: LevelScaling
 
-UnlockCriteria.update_forward_refs()
+Trace = Union[Skill, StatBonus, BonusAbility]
+   
+
+
+
+UnlockPrerequisite.update_forward_refs()
 
 
