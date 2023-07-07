c4a1550235138ff483c4d304bc139b1349bf157b
heko <91366057+hkohko@users.noreply.github.com>
2023-07-07 11:04:47 +0700

    new attributes

--- data_query/lightcones_data.py
+++ data_query/lightcones_data.py
@@ -1,8 +1,9 @@
 import json
-import os
-from typing import Dict, List, Any
+import re
+from itertools import count
+from typing import Dict, List, Any, Iterator, Tuple
 from data_query.shared_data.shared_var import SharedVar
-from data_query.query_errors.error_msg import QueryError
+from data_query.query_errors.errors import *
 
 
 class LightCone:
@@ -22,18 +23,53 @@ class LightCone:
     def level_data(self) -> List[Dict]:
         return self.content["levelData"]
 
-    def level_data_onlevel(self, level: int = 80) -> Dict | str | None:
+    def level_data_onlevel(self, level: int = 80) -> Dict :
         level_list: List[int] = SharedVar.level()
         levelData: List[Dict] = self.level_data()
         if level in level_list:
             for data in levelData:
                 if data["maxLevel"] == level:
                     return data
-                else:
-                    return None
         else:
-            return QueryError.leveldata_outofrange()
+            raise LevelOutOfRangeError
 
-    def skill(self) -> Dict:
-        skills_data = self.content["skill"]
+    def skill_data(self) -> Dict:
+        skills_data: Dict = self.content["skill"]
         return skills_data
+
+    def skill_data_onlevel(self, level: int = 5) -> Dict:
+        level_iterator: Iterator = count(start=1, step=1)
+        level_list: List[int] = list(next(level_iterator) for _ in range(5))
+        if level in level_list and isinstance(level, int):
+            levelData: List[Dict] = self.skill_data()["levelData"]
+            for data in levelData:
+                if data["level"] == level:
+                    return data
+        else:
+            raise LightConeSkillLevelOutOfRange
+
+    def skill_descHash(self, level: int = 5) -> str:
+        skill_onlevel_data: Dict | str = self.skill_data_onlevel(level)
+        if isinstance(skill_onlevel_data, str):
+            return skill_onlevel_data
+        descHash: str = self.skill_data()["descHash"]
+        name: str = self.skill_data()["name"]
+        skill_params: List = skill_onlevel_data["params"]
+        descHash_cleaned: str = re.sub("<[^\>]+.", "", descHash)
+        value_params: Tuple = tuple(
+            f"{value * 100:.1f}" if isinstance(value, float) else str(value)
+            for value in skill_params
+        )
+        descHash_list: List = []
+        for index in range(len(skill_params)):
+            if len(descHash_list) == 0:
+                descHash_first: str = descHash_cleaned.replace(
+                    f"#{index+1}[i]", value_params[index]
+                )
+                descHash_list.append(descHash_first)
+            else:
+                descHash_next: str = descHash_list[index - 1].replace(
+                    f"#{index+1}[i]", value_params[index]
+                )
+                descHash_list.append(descHash_next)
+        return f"{name} Lv.{level}: {descHash_list[-1]}"
