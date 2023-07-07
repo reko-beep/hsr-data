b499a64a23078bfaf26dc5afa9d36e05016534e9
heko <91366057+hkohko@users.noreply.github.com>
2023-07-07 11:05:13 +0700

    custom errors

--- data_query/query_errors/errors.py
+++ data_query/query_errors/errors.py
@@ -1,13 +1,20 @@
 class LevelOutOfRangeError(Exception):
-    msg = "defaultvalue: 80\nLevelOutOfRange: " \
-          "level must be int and in range 20-80 with increments of 10."
+    msg = (
+        "defaultvalue: 80\nLevelOutOfRange: "
+        "level must be int and in range 20-80 with increments of 10."
+    )
+
     def __init__(self, message=msg):
         self.message = message
         super().__init__(self.message)
 
+
 class LightConeSkillLevelOutOfRange(Exception):
-    msg = "defaultvalue: 5\nLightconeSkillLevelError: " \
-          "level must be int and in range 1-5 with increments of 1."
+    msg = (
+        "defaultvalue: 5\nLightconeSkillLevelError: "
+        "level must be int and in range 1-5 with increments of 1."
+    )
+
     def __init__(self, message=msg):
         self.message = message
         super().__init__(self.message)
