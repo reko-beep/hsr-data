4301487746c9e2710eda4f33cab1733d91359406
heko <91366057+hkohko@users.noreply.github.com>
2023-07-13 08:18:24 +0700

    materials_data test cases

---
+++ tests/materials_data_test.py
@@ -0,0 +1,32 @@
+import unittest
+import sqlite3 as sql
+from data_query.materials_data import Materials
+from dotenv import dotenv_values
+
+key = dotenv_values(".env")
+conn = sql.connect(key["FSEARCH_DB_LOCATION"])
+
+
+class TestMaterials(unittest.TestCase):
+    def test_name(self):
+        cursor = conn.cursor()
+        cursor.execute("SELECT id, name from mats_names")
+        for mats_id, name in cursor:
+            self.assertEqual(Materials(mats_id).name(), name)
+
+    def test_id(self):
+        cursor = conn.cursor()
+        cursor.execute("SELECT id, name from mats_names")
+        for mats_id, name in cursor:
+            self.assertEqual(Materials(mats_id).id(), mats_id)
+
+    def test_rarity(self):
+        cursor = conn.cursor()
+        cursor.execute("SELECT id, rarity from mats_names")
+        for mats_id, rarity in cursor:
+            self.assertEqual(Materials(mats_id).rarity(), rarity)
+
+
+if __name__ == "__main__":
+    unittest.main()
+    conn.close()
