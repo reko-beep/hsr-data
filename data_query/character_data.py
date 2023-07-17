8f93d2e6a700022dc602689d2d95325f9befc231
heko <91366057+hkohko@users.noreply.github.com>
2023-07-17 08:25:57 +0700

    Updated Character trace
    fixed obsolete import
    Updated traces, fixed import

--- data_query/character_data.py
+++ data_query/character_data.py
@@ -6,10 +6,10 @@ import sqlite3
 from dotenv import dotenv_values
 from pathlib import Path
 from collections import defaultdict
-from typing import Dict, Generator, Tuple, List, Any
+from typing import Generator, Any
 from itertools import count
 from data_query.query_errors.errors import *
-from data_query.character_module.traces_embed_buff import TracesEmbedBuff
+import data_query.character_module.trace_module as TraceModule
 import data_query.shared_data.shared_var as SharedVar
 import data_query.character_module.skill_description as skill_desc
 
@@ -20,9 +20,9 @@ class Character:
             sys.exit("Missing 1 argument: name of character")
         os.chdir(Path(__file__).parent.parent)
         with open(f"raw_data/en/characters/{name}.json") as file:
-            self.content: Dict = json.loads(file.read())
+            self.content: dict = json.loads(file.read())
 
-    def json_data(self) -> List[str]:
+    def json_data(self) -> list[str]:
         """
         Returns a list containing available data from the character.
 
@@ -64,7 +64,7 @@ class Character:
         """
         return self.content["baseType"]["name"]
 
-    def stat_data_onlevel(self, level: int = 80) -> Dict | str:  # type: ignore
+    def stat_data_onlevel(self, level: int = 80) -> dict | str:  # type: ignore
         """
         Returns character's base stat on every ascension.
 
@@ -73,60 +73,24 @@ class Character:
         level_list = SharedVar.level()
         if level not in level_list or isinstance(level, int) == False:
             raise LevelOutOfRangeError
-        stat_dict: List[Dict] = self.content["levelData"]
+        stat_dict: list[dict] = self.content["levelData"]
         for data in stat_dict:
             if data["maxLevel"] == level:
                 return data
 
     def trace(self) -> dict:
         """
-        Returns a generator object containing character's Traces data
+        Returns a dictionary containing character's Traces data
 
         trace = Character("character_name").trace())
-        for data in trace:
-            print(data)
+        print(trace)
         """
-        traces_data: List = self.content.get("skillTreePoints")
-        embed_buffs = TracesEmbedBuff(self.content)
-        named_traceslist = []
-        embedskill_buffs = defaultdict(list)
-        for index, data in enumerate(traces_data):
-            trace_skill = data.get("embedBonusSkill")
-            children1 = data.get("children")
-            if trace_skill is None:
-                continue
-            name = trace_skill.get("name")
-            deschash = trace_skill.get("descHash")
-            level_data = trace_skill.get("levelData")
-            for data in level_data:
-                level = data.get("level")
-                params = data.get("params")
-            output = "trace"
-            readable_deschash = SharedVar.readable_descHash(
-                name, params, deschash, level, output
-            )
-            readable_deschash_punctuations = SharedVar.correct_punctuations(
-                readable_deschash
-            )
-            named_traceslist.append((readable_deschash_punctuations, children1))
-        for data in named_traceslist:
-            name: str = data[0]
-            infos: list = data[1]  # list with length 1 :)
-            for info in infos:
-                childrens: dict = info
-                embed_buff0 = childrens.get("embedBuff")
-                embedskill_buffs[name] += [embed_buff0]
-                children1 = childrens.get("children")
-                for data1 in children1:
-                    embed_buff1 = data1.get("embedBuff")
-                    embedskill_buffs[name] += [embed_buff1]
-                    children2 = data1.get("children")
-                    if len(children2) == 0:
-                        embed_buff2 = None
-                    for data2 in children2:
-                        embed_buff2 = data2.get("embedBuff")
-                        embedskill_buffs[name] += [embed_buff2]
-        return embedskill_buffs
+        named_traceslist = TraceModule.trace_namedskills(self.content)
+        trace_data = TraceModule.trace_embedbuffs(named_traceslist)
+        unnamed_buff = TraceModule.skilltreepoints_embedbuff_children0(self.content)
+        list_unnamedbuff = [buff for buff in unnamed_buff]
+        trace_data["UnnamedBuff"] = list_unnamedbuff
+        return trace_data
 
     def constellation(
         self,
@@ -138,11 +102,11 @@ class Character:
         for data in constellaton:
             print(data)
         """
-        const_data: List = self.content["ranks"]
+        const_data: list = self.content["ranks"]
         for data in const_data:
             const_name: str = data.get("name")
             const_desc: str = data.get("descHash")
-            const_params: List[float] = data.get("params")
+            const_params: list[float] = data.get("params")
             output: str = "constellation"
             descHash: str | None = SharedVar.readable_descHash(
                 const_name, const_params, const_desc, "Max", output
